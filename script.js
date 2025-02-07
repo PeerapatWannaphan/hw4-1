198c3dfb997d8b894249dec720941e23187063c2
Peerapat Wannaphan <peerapatwannaphan@gmail.com>
2025-02-08 00:24:43 +0700

    จัดการ Local Storage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("expense-form");
     const expenseList = document.getElementById("expense-list");
+    
+    function getExpenses() {
+        return JSON.parse(localStorage.getItem("expenses")) || [];
+    }
